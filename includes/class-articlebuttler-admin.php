97c7d154da48d56438f3be2ed96aab5d76c73b98
Zeus-Eternal <121900625+Zeus-Eternal@users.noreply.github.com>
2023-07-15 17:51:29 -0400

    Create class-articlebuttler-admin.php

---
+++ includes/class-articlebuttler-admin.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * ArticleButtler_Admin Class
+ *
+ * Handles the administration functionality of the ArticleButtler plugin.
+ */
+class ArticleButtler_Admin {
+    /**
+     * Initialize the admin functionality.
+     */
+    public function init() {
+        // Add the plugin settings page.
+        add_action('admin_menu', array($this, 'add_settings_page'));
+
+        // Register the plugin settings.
+        add_action('admin_init', array($this, 'register_settings'));
+    }
+
+    /**
+     * Add the settings page to the admin menu.
+     */
+    public function add_settings_page() {
+        // Add a new top-level menu page.
+        add_menu_page(
+            'ArticleButtler Settings',
+            'ArticleButtler',
+            'manage_options',
+            'articlebuttler-settings',
+            array($this, 'render_settings_page'),
+            'dashicons-admin-generic',
+            80
+        );
+    }
+
+    /**
+     * Render the plugin settings page.
+     */
+    public function render_settings_page() {
+        // Add your settings page HTML and form rendering logic here.
+        // Customize this method to display the desired settings fields and options.
+    }
+
+    /**
+     * Register the plugin settings.
+     */
+    public function register_settings() {
+        // Register your plugin settings here.
+        // Customize this method to add the necessary settings fields and sections.
+    }
+}
+
+// Instantiate the ArticleButtler_Admin class.
+$article_buttler_admin = new ArticleButtler_Admin();
+$article_buttler_admin->init();
