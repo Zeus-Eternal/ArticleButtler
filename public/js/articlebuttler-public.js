f81ae605a275cc734a830fbc43871da206363599
Zeus-Eternal <121900625+Zeus-Eternal@users.noreply.github.com>
2023-07-15 15:26:29 -0400

    Create articlebuttler-public.js
    The articlebuttler-public.js script is for handling the generation of articles in the ArticleButtler public interface. It includes a jQuery document ready function that binds a click event handler to the generate button.
    
    When the generate button is clicked, it retrieves the prompt input value, performs an AJAX request to the server, and expects a JSON response. The AJAX request includes the necessary data such as the action, prompt, and nonce.
    
    Upon a successful response, the generated article is displayed in the .articlebuttler-generated-content element. If the response indicates an error, an error message is displayed.

---
+++ public/js/articlebuttler-public.js
@@ -0,0 +1,42 @@
+/**
+ * ArticleButtler Public JS
+ */
+jQuery(document).ready(function($) {
+    // Handle generate button click event
+    $('.articlebuttler-generate-button').on('click', function() {
+        // Get the prompt input value
+        var prompt = $('.articlebuttler-prompt-input').val();
+
+        // Perform AJAX request to generate article
+        $.ajax({
+            url: articlebuttler_public_vars.ajaxurl,
+            type: 'POST',
+            dataType: 'json',
+            data: {
+                action: 'articlebuttler_generate_article',
+                prompt: prompt,
+                nonce: articlebuttler_public_vars.nonce
+            },
+            beforeSend: function() {
+                // Show loading spinner or any other UI indication
+            },
+            success: function(response) {
+                // Process the response
+                if (response.success) {
+                    // Display the generated article
+                    $('.articlebuttler-generated-content').html(response.article);
+                } else {
+                    // Display an error message
+                    alert('Failed to generate the article. Please try again.');
+                }
+            },
+            error: function(xhr, textStatus, errorThrown) {
+                // Display an error message
+                alert('Failed to generate the article. Please try again.');
+            },
+            complete: function() {
+                // Hide loading spinner or any other UI indication
+            }
+        });
+    });
+});
